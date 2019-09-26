15e95dd8a4f3eb76fb8bfbce128932a4a7b76ff4
Dave Pemberton <dave.pemberton@softwareag.com>
2019-09-26 20:03:04 +0100

    new

---
+++ deploycheck.groovy
@@ -0,0 +1,32 @@
+//input(message: 'Release Service?', id: 'release')
+        userAborted = false
+        startMillis = System.currentTimeMillis()
+        timeoutMillis = 10000
+
+        try {
+          timeout(time: timeoutMillis, unit: 'MILLISECONDS') {
+            input 'Do you approve?'
+          }
+        } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+          cause = e.causes.get(0)
+          echo "Aborted by " + cause.getUser().toString()
+          if (cause.getUser().toString() != 'SYSTEM') {
+            startMillis = System.currentTimeMillis()
+          } else {
+            endMillis = System.currentTimeMillis()
+            if (endMillis - startMillis >= timeoutMillis) {
+              echo "Approval timed out. Continuing with deployment."
+            } else {
+              userAborted = true
+              echo "SYSTEM aborted, but looks like timeout period didn't complete. Aborting."
+            }
+          }
+        }
+
+        if (userAborted) {
+          currentBuild.result = 'ABORTED'
+        } else {
+          currentBuild.result = 'SUCCESS'
+          echo "Firing the missiles!"
+        }
+      }
