e20e4f948265a546925ff78b85803700c79171fe
Dave Pemberton <dave.pemberton@softwareag.com>
2019-09-29 20:21:29 +0100

    collect version number

---
+++ versionInput.groovy
@@ -0,0 +1,4 @@
+userInput = input(
+id: 'Version Required', message: 'Please provide version number', parameters: [
+[$class: 'StringParameterDefinition', defaultValue: "0.0.0", description: '', name: 'Version Number']
+])
