140d2cbaa5f5bbb67e7845ced6f42140e7948470
Dave Pemberton (ukDXP) <Dave.Pemberton@softwareag.com>
2019-09-30 14:46:26 +0100

    pull api from server

---
+++ pullProductAPI.bat
@@ -0,0 +1,7 @@
+set username=Administrator
+set password=manage
+set url=http://apiworldbuild:5555
+set apiID=dda7a194-dced-4c2f-912f-4bd85a3ba965
+set file=product.zip
+
+curl -u %username%:%password% %url%/rest/apigateway/archive?apis=%apiID% --output %file%
