c72b12e62509ddcf0f486006a042f81327c1dbcc
Dave Pemberton (ukDXP) <Dave.Pemberton@softwareag.com>
2019-09-30 19:36:54 +0100

    modified groovy

--- versionInput.groovy
+++ versionInput.groovy
@@ -1,8 +1,15 @@
-userInput = input(
-id: 'Version Required', message: 'Please provide version number', parameters: [
-[$class: 'StringParameterDefinition', defaultValue: "0.0.0", description: '', name: 'Version Number']
-])
 
+if(env.GIT_BRANCH!="developmen")
+{
+    userInput = input(
+    id: 'Version Required', message: 'Please provide version number', parameters: [
+    [$class: 'StringParameterDefinition', defaultValue: "0.0.0", description: '', name: 'Version Number']
+    ])
+}
+else
+{
+    userInput="ci"
+}
 env.VERSION=userInput
 
 //build.setDisplayName("ReleaseBuild v" + userInput)
